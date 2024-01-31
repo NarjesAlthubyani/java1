52f0c5ceb23d5392df2a34df06d0068e34d792b6
Narjes <narjes.jh12@gmail.com>
2024-01-31 11:46:51 +0300

    lab1 in CPIT251

---
+++ src/lab1_251/Lab1_251.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1_251;
+
+/**
+ *
+ * @author 2205805
+ */
+public class Lab1_251 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello github");
+    }
+    
+}
